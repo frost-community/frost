3ecc1e0289385a7e98bdab6bd940e346d6abd5a9
marihachi <marihachi0620@gmail.com>
2019-11-16 16:53:25 +0900

    allow to instantiate ServerEngine

--- server/src/engine/ServerEngine.ts
+++ server/src/engine/ServerEngine.ts
@@ -18,27 +18,33 @@ function log(...params: any[]) {
 	console.log('[ServerEngine]', ...params);
 }
 
-export interface ServerContext {
-	components: IComponent[];
-	db: MongoProvider;
-	messenger: EventEmitter;
-	setupItems: SetupItem[];
-}
-
 export default class ServerEngine {
-	private static async install(ctx: ServerContext, component: IComponent, bootConfig: IBootConfig): Promise<void> {
+	private components: IComponent[];
+	private db?: MongoProvider;
+	private messenger: EventEmitter;
+	private setupItems: SetupItem[];
+
+	constructor() {
+		this.components = [];
+		this.messenger = new EventEmitter();
+		this.setupItems = [];
+	}
+
+	private async install(component: IComponent, bootConfig: IBootConfig): Promise<void> {
+		if (!this.db) throw new Error('need to start the ServerEngine');
 		if (component.install) {
 			log(`installing: ${component.name}`);
-			await component.install(new InstallApi(component, ctx.db, ctx.setupItems, bootConfig));
+			await component.install(new InstallApi(component, this.db, this.setupItems, bootConfig));
 		}
 	}
 
-	private static async boot(ctx: ServerContext, component: IComponent, bootConfig: IBootConfig): Promise<void> {
+	private async boot(component: IComponent, bootConfig: IBootConfig): Promise<void> {
+		if (!this.db) throw new Error('need to start the ServerEngine');
 		log(`booting: ${component.name}`);
-		await component.boot(new BootApi(component, ctx.db, ctx.messenger, bootConfig));
+		await component.boot(new BootApi(component, this.db, this.messenger, bootConfig));
 	}
 
-	static async start(bootConfigFilepath: string): Promise<void> {
+	async start(bootConfigFilepath: string): Promise<void> {
 		// option args
 		argv.option({
 			name: 'serverSetting',
@@ -109,13 +115,6 @@ export default class ServerEngine {
 			throw new Error('please generate a valid boot config on the server setting menu.');
 		}
 
-		const ctx: ServerContext = {
-			db,
-			components: [],
-			messenger: new EventEmitter(),
-			setupItems: []
-		};
-
 		for (const componentName of bootConfig.usingComponents) {
 			if (!/^[a-z0-9_-]+$/i.test(componentName)) {
 				throw new Error(`invalid component name: ${componentName}`);
@@ -138,24 +137,23 @@ export default class ServerEngine {
 			if (!verifyComponent(component)) {
 				throw new Error(`failed to load ${componentName} component`);
 			}
-			ctx.components.push(component);
+			this.components.push(component);
 		}
 
-		for (const component of ctx.components) {
-			ServerEngine.install(ctx, component, bootConfig);
+		for (const component of this.components) {
+			this.install(component, bootConfig);
 		}
 
 		if (options.componentSetting) {
-			showComponentSettingMenu(ctx.setupItems);
+			showComponentSettingMenu(this.setupItems);
 			if (db) {
 				await db.disconnect();
 			}
 			return;
 		}
 
-		for (const component of ctx.components) {
-			ServerEngine.boot(ctx, component, bootConfig);
+		for (const component of this.components) {
+			this.boot(component, bootConfig);
 		}
 	}
-
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -9,8 +9,8 @@ async function entryPoint() {
 	console.log('===========');
 
 	const bootConfigPath = path.resolve(__dirname, '../.configs/boot-config.json');
-
-	await ServerEngine.start(bootConfigPath);
+	const engine = new ServerEngine();
+	await engine.start(bootConfigPath);
 }
 
 entryPoint()
