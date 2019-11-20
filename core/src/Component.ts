2b29a908b24988c37e1daadc5665c08d9acf98c1
Marihachi <marihachi0620@gmail.com>
2019-11-20 12:33:15 +0900

    remove action, event

--- core/src/Component.ts
+++ core/src/Component.ts
@@ -2,28 +2,6 @@ import $ from 'cafy';
 import MongoProvider from './MongoProvider';
 import ConsoleMenu from './ConsoleMenu';
 
-export type ActionResult = ActionOkResult | ActionErrorResult;
-
-export interface ActionOkResult {
-	data: {[x: string]: any};
-}
-export function isActionOkResult(actionResult: {[x: string]: any}): actionResult is ActionOkResult {
-	return actionResult.data != undefined;
-}
-
-export interface ActionError {
-	reason: string;
-	details?: any;
-}
-
-export interface ActionErrorResult {
-	error: ActionError;
-}
-export function isActionErrorResult(actionResult: {[x: string]: any}): actionResult is ActionErrorResult {
-	return actionResult.error != undefined;
-}
-
-
 export interface IComponentInstallApi {
 	cryptoKey: string;
 	db: MongoProvider;
@@ -33,17 +11,6 @@ export interface IComponentInstallApi {
 export interface IComponentBootApi {
 	cryptoKey: string;
 	db: MongoProvider;
-
-	// action
-	defineAction(actionName: string, handler: (data: {[x: string]: any}) => Promise<ActionResult>): void;
-	callAction(componentName: string, actionName: string, data: {[x: string]: any}, options?: { timeout?: number }): Promise<ActionResult>;
-
-	// event
-	emitEvent(componentName: string, eventType: string, eventData: {[x: string]: any}): void;
-	addEventListener(eventType: string, callback: (eventData: {[x: string]: any}) => void): void;
-	removeEventListener(eventType: string, callback: (eventData: {[x: string]: any}) => void): void;
-	removeAllEventListeners(eventType?: string): void;
-
 	use(componentName: string): any;
 }
 
