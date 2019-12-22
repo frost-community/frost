83afde0af6a717770f9ae493894e77be7ef5b1de
marihachi <marihachi0620@gmail.com>
2019-12-22 17:43:15 +0900

    error handling for input

--- core/src/inputLine.ts
+++ core/src/inputLine.ts
@@ -1,11 +1,16 @@
 import readLine from 'readline';
 
 export default function(message: string): Promise<string> {
-	return new Promise<string>((resolve) => {
+	return new Promise<string>((resolve, reject) => {
 		const rl = readLine.createInterface(process.stdin, process.stdout);
 		rl.question(message, (ans) => {
-			resolve(ans);
 			rl.close();
+			resolve(ans);
+		});
+		rl.on('SIGINT', () => {
+			console.log('');
+			rl.close();
+			reject('input stopped');
 		});
 	});
 }
