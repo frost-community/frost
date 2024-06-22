3c870846fe2f94eebbc190726a68b30604e72afc
Simirall <37952374+Simirall@users.noreply.github.com>
2024-06-22 14:43:54 +0000

    react vite init

---
+++ frontend/src/pages/App.tsx
@@ -0,0 +1,9 @@
+import deleteParrot from "/deletedparrot.gif";
+
+export const App = () => {
+  return (
+    <>
+      <img src={deleteParrot} />
+    </>
+  );
+};
---
+++ frontend/vite.config.ts
@@ -0,0 +1,11 @@
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    host: "0.0.0.0",
+  },
+});
