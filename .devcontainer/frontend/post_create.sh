8548be0a4099a8125d3c583b933fddd3747a2a6e
しなちくシステム <git@thinaticsystem.com>
2024-06-22 17:23:40 +0900

    Environment: Devcontainer (#39)
    Co-authored-by: marihachi <marihachi0620@gmail.com>

---
+++ .devcontainer/frontend/post_create.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Bash Strict Mode (http://redsymbol.net/articles/unofficial-bash-strict-mode/)
+set -euo pipefail
+IFS=$'\n\t'
+
+# Constants
+USER_NAME=vscode
+GROUP_NAME=vscode
+WORKSPACE_DIR=/workspaces/frost
+BUILT_NODE_MODULES_DIR=/tmp/project_dependencies/node_modules
+NODE_MODULES_DESTINATION_PATH="${WORKSPACE_DIR}/frontend/node_modules"
+
+# Dockerイメージでビルド済みの node_modules/ をワークスペースへコピー
+# ローカルで作成されたnode_modulesがあったら削除しておく
+if [ -e "$NODE_MODULES_DESTINATION_PATH" ]; then
+  sudo unlink "$NODE_MODULES_DESTINATION_PATH"
+fi
+sudo ln --symbolic "$BUILT_NODE_MODULES_DIR" "$NODE_MODULES_DESTINATION_PATH"
+sudo chown "${USER_NAME}:${GROUP_NAME}" --recursive "$NODE_MODULES_DESTINATION_PATH"
+
