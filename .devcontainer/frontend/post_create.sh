50a8c80bff6c9dc6ae5204883bef07d5e8d9806b
しなちくシステム <git@thinaticsystem.com>
2024-06-22 18:34:20 +0900

    Fix: Dev Container (#43)

--- .devcontainer/frontend/post_create.sh
+++ .devcontainer/frontend/post_create.sh
@@ -6,13 +6,13 @@ IFS=$'\n\t'
 # Constants
 USER_NAME=vscode
 GROUP_NAME=vscode
-WORKSPACE_DIR=/workspaces/frost
+WORKSPACE_DIR=/workspaces/Frost
 BUILT_NODE_MODULES_DIR=/tmp/project_dependencies/node_modules
 NODE_MODULES_DESTINATION_PATH="${WORKSPACE_DIR}/frontend/node_modules"
 
 # Dockerイメージでビルド済みの node_modules/ をワークスペースへコピー
 # ローカルで作成されたnode_modulesがあったら削除しておく
-if [ -e "$NODE_MODULES_DESTINATION_PATH" ]; then
+if [[ -e "$NODE_MODULES_DESTINATION_PATH" || -L "$NODE_MODULES_DESTINATION_PATH" ]]; then
   sudo unlink "$NODE_MODULES_DESTINATION_PATH"
 fi
 sudo ln --symbolic "$BUILT_NODE_MODULES_DIR" "$NODE_MODULES_DESTINATION_PATH"
