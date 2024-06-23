56f5db06ed6e66b245b170386832d971c578a46a
marihachi <marihachi0620@gmail.com>
2024-06-24 00:46:32 +0900

    node_modulesをvolumeマウントする試み (#60)

--- .devcontainer/frontend/post_create.sh
+++ .devcontainer/frontend/post_create.sh
@@ -8,22 +8,12 @@ IFS=$'\n\t'
 USER_NAME=vscode
 GROUP_NAME=vscode
 PROJECT_DIR=/workspaces/Frost
-BUILT_NODE_MODULES_DIR=/tmp/project_dependencies/node_modules
 NODE_MODULES_DESTINATION_PATH="${PROJECT_DIR}/frontend/node_modules"
 
-# Dockerイメージでビルド済みの node_modules/ をワークスペースへ移動
-# TODO: symlink時代の対応 近いうちに消す
-if [ -L "$NODE_MODULES_DESTINATION_PATH" ]; then
-  sudo unlink "$NODE_MODULES_DESTINATION_PATH"
-fi
-if [[ ! -d "$BUILT_NODE_MODULES_DIR" ]]; then
+# 生成されたnode_modulesの所有者を変更
+if [[ ! -d "$NODE_MODULES_DESTINATION_PATH" ]]; then
   # node_modules/がない場合はnop
   echo No "node_modules/"
 else
-  # 過去に作成されたnode_modulesがあったら削除しておく
-  if [ -d "$NODE_MODULES_DESTINATION_PATH" ]; then
-    sudo rm --recursive  "$NODE_MODULES_DESTINATION_PATH"
-  fi
-  sudo mv "$BUILT_NODE_MODULES_DIR" "$NODE_MODULES_DESTINATION_PATH"
   sudo chown "${USER_NAME}:${GROUP_NAME}" --recursive "$NODE_MODULES_DESTINATION_PATH"
 fi
