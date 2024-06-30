2d476e0388ef64f0d1f3cef549434045aa73d12b
Simirall <37952374+Simirall@users.noreply.github.com>
2024-06-30 08:51:01 +0000

    mantine init

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
+import { MantineProvider } from "@mantine/core";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { App } from "./pages/App.tsx";
+import "@mantine/core/styles.css";
+import { themeConfig } from "./style/themeConfig.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider theme={themeConfig} defaultColorScheme="auto">
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
 );
--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,9 +1,32 @@
+import { Button, Image, useMantineColorScheme } from "@mantine/core";
+
 import deleteParrot from "/deletedparrot.gif";
 
+import { useToggle } from "@mantine/hooks";
+
 export const App = () => {
+  const [deleted, toggleDeleted] = useToggle();
+  const { toggleColorScheme } = useMantineColorScheme();
+
   return (
     <>
-      <img src={deleteParrot} />
+      <Image
+        src={deleteParrot}
+        h={100}
+        w="auto"
+        style={{
+          visibility: deleted ? "hidden" : "visible",
+        }}
+      />
+      <Button
+        color="red"
+        onClick={() => {
+          toggleDeleted();
+        }}
+      >
+        DeleteParrotをDelete
+      </Button>
+      <Button onClick={toggleColorScheme}>Change DarkMode</Button>
     </>
   );
 };
