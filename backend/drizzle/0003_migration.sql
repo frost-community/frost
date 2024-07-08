2d046d927dee46e4fe8e41fb3f98865a561e7ad9
marihachi <marihachi0620@gmail.com>
2024-07-08 12:42:24 +0000

    remove db migrations

--- backend/drizzle/0003_migration.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE "account" ALTER COLUMN "name" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "account" ALTER COLUMN "passwordAuthEnabled" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "passwordAuth" ALTER COLUMN "id" SET DEFAULT gen_random_uuid();--> statement-breakpoint
-ALTER TABLE "passwordAuth" ALTER COLUMN "algorithm" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "passwordAuth" ALTER COLUMN "salt" SET DATA TYPE varchar(32);--> statement-breakpoint
-ALTER TABLE "passwordAuth" ALTER COLUMN "salt" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "passwordAuth" ALTER COLUMN "hash" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "user" ALTER COLUMN "name" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "user" ALTER COLUMN "displayName" SET NOT NULL;
