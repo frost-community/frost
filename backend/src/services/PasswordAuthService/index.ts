8f06534a5ac4731420492a430e23c90d1208f3c3
marihachi <marihachi0620@gmail.com>
2024-07-10 15:06:17 +0000

    service wip

--- backend/src/services/PasswordAuthService/index.ts
+++ backend/src/services/PasswordAuthService/index.ts
@@ -1,4 +1,8 @@
+import { Container, inject, injectable } from 'inversify';
+import { TYPES } from '../../types';
+import { DatabaseService } from '../DatabaseService';
 
+@injectable()
 export class PasswordAuthService {
   constructor() {}
 }
