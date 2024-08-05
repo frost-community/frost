8646f716aa9e1e93adda8bebf0c9d9b830e0280e
marihachi <marihachi0620@gmail.com>
2024-08-05 22:37:12 +0000

    http error

--- backend/src/modules/service-error.ts
+++ backend/src/modules/service-error.ts
@@ -1,15 +1,18 @@
-export type ErrorCode =
-  | 'internalServerError'
-  | 'endpointNotFound'
-  | 'userNotFound'
-  | 'accountNotFound';
-
-const errorInfo: Record<ErrorCode, { name: string, status: number }> = {
+export const errorInfo = {
+  // http standard
   internalServerError: { name: 'Internal Server Error', status: 500 },
+  methodNotAllowed: { name: 'Method Not Allowed', status: 405 },
+  notFound: { name: 'Not Found', status: 404 },
+  forbidden: { name: 'Forbidden', status: 403 },
+  unauthorized: { name: 'Unauthorized', status: 401 },
+  badRequest: { name: 'Bad Request', status: 400 },
+  // application defined
   endpointNotFound: { name: 'Endpoint Not Found', status: 404 },
   accountNotFound: { name: 'Account Not Found', status: 404 },
   userNotFound: { name: 'User Not Found', status: 404 },
-};
+} satisfies Record<string, { name: string, status: number }>;
+
+export type ErrorCode = keyof typeof errorInfo;
 
 export interface IServiceError {
   errorCode?: ErrorCode;
@@ -17,7 +20,7 @@ export interface IServiceError {
   status: number;
 }
 
-export class ServiceError extends Error {
+export class ServiceError extends Error implements IServiceError {
   constructor(
     name: string,
     public status: number,
