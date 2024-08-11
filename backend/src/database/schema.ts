e2f8e2feab87753410f0123667e77eb815d6bcf4
marihachi <marihachi0620@gmail.com>
2024-08-11 21:14:02 +0000

    update database schema

--- backend/src/database/schema.ts
+++ backend/src/database/schema.ts
@@ -3,9 +3,9 @@ import * as D from 'drizzle-orm/pg-core';
 // account
 
 export const Account = D.pgTable('account', {
-  accountId: D.uuid('accountId').primaryKey().defaultRandom(),
+  accountId: D.uuid('account_id').primaryKey().defaultRandom(),
   name: D.varchar('name', { length: 64 }).notNull(),
-  passwordAuthEnabled: D.boolean('passwordAuthEnabled').notNull(),
+  passwordAuthEnabled: D.boolean('password_auth_enabled').notNull(),
 });
 
 export type AccountRow = typeof Account.$inferSelect;
@@ -14,9 +14,9 @@ export type AccountInsertParams = typeof Account.$inferInsert;
 
 // password verification
 
-export const PasswordVerification = D.pgTable('passwordVerification', {
-  passwordVerificationId: D.uuid('passwordVerificationId').primaryKey().defaultRandom(),
-  accountId: D.uuid('accountId').notNull(),
+export const PasswordVerification = D.pgTable('password_verification', {
+  passwordVerificationId: D.uuid('password_verification_id').primaryKey().defaultRandom(),
+  accountId: D.uuid('account_id').notNull(),
   algorithm: D.varchar('algorithm', { length: 16 }).notNull(),
   salt: D.varchar('salt', { length: 32 }).notNull(),
   iteration: D.integer('iteration').notNull(),
@@ -30,10 +30,10 @@ export type PasswordVerificationInsertParams = typeof PasswordVerification.$infe
 // user
 
 export const User = D.pgTable('user', {
-  userId: D.uuid('userId').primaryKey().defaultRandom(),
-  accountId: D.uuid('accountId').notNull(),
+  userId: D.uuid('user_id').primaryKey().defaultRandom(),
+  accountId: D.uuid('account_id').notNull(),
   name: D.varchar('name', { length: 64 }).unique().notNull(),
-  displayName: D.varchar('displayName', { length: 64 }).default('frost user').notNull(),
+  displayName: D.varchar('display_name', { length: 64 }).default('frost user').notNull(),
 });
 
 export type UserRow = typeof User.$inferSelect;
