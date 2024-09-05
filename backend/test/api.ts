a49d04c60b35a0a3bb94146ab4ccf70f3f475608
marihachi <marihachi0620@gmail.com>
2024-09-06 04:10:26 +0900

    api test (#122)

--- backend/test/api.ts
+++ backend/test/api.ts
@@ -1,21 +1,52 @@
 import assert from "assert";
+import e from "express";
+import { Server } from "http";
 import { Container } from "inversify";
 import request from 'supertest';
+import { AppConfig } from "../src/app";
 import { setupContainer } from "../src/container/inversify.config";
-import { App } from "../src/app";
 import { TYPES } from "../src/container/types";
+import { createHttpServer } from "../src/modules/httpServer";
+
+function listen(app: e.Express, port: number): Promise<Server> {
+  return new Promise<Server>(resolve => {
+    const s = app.listen(port, () => resolve(s));
+  });
+}
+
+async function setup() {
+  const container = new Container();
+  setupContainer(container);
+  const app = await createHttpServer(container);
+  const config = container.get<AppConfig>(TYPES.AppConfig);
+  const server = await listen(app, config.port);
+
+  return { server };
+}
 
 describe('api', () => {
-
   it('GET /api/v1/echo', async () => {
-    // const container = new Container();
-    // setupContainer(container);
-  
-    // const app = container.get<App>(TYPES.App);
-    // const server = await app.run();
+    const { server } = await setup();
+    try {
+      await request(server)
+        .get('/api/v1/echo?message=abcxyz')
+        .expect(200, { message: 'abcxyz' });
+    }
+    finally {
+      server.close();
+    }
+  });
 
-    // request(server)
-    //   .GET('/api/v1/echo?message=123')
-    //   .expect(200);
+  it('POST /api/v1/echo', async () => {
+    const { server } = await setup();
+    try {
+      await request(server)
+        .post('/api/v1/echo')
+        .send({ message: 'abc123' })
+        .expect(200, { message: 'abc123' });
+    }
+    finally {
+      server.close();
+    }
   });
 });
