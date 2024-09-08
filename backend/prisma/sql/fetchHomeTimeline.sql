b9c8383ab337bebe93853afc32d7e6e9e30a94da
marihachi <marihachi0620@gmail.com>
2024-09-08 16:30:53 +0000

    timeline expt

--- backend/prisma/sql/fetchHomeTimeline.sql
+++ backend/prisma/sql/fetchHomeTimeline.sql
@@ -5,9 +5,10 @@ SELECT
   p.post_id,
   p.chat_room_id,
   p.user_id,
-  p.content
+  p.content,
+  p.created_at
 FROM post AS p
 WHERE p.user_id = CAST($1 AS UUID)
   AND p.chat_room_id IS NULL
---ORDER BY p.created_at DESC
+ORDER BY p.created_at ASC
 LIMIT $2
--- backend/prisma/sql/fetchHomeTimelineCursor.sql
+++ backend/prisma/sql/fetchHomeTimelineCursor.sql
@@ -6,10 +6,11 @@ SELECT
   p.post_id,
   p.chat_room_id,
   p.user_id,
-  p.content
+  p.content,
+  p.created_at
 FROM post AS p
 WHERE p.user_id = CAST($1 AS UUID)
   AND p.chat_room_id IS NULL
   AND CAST($2 AS UUID) = CAST($2 AS UUID)
---ORDER BY p.created_at DESC
+ORDER BY p.created_at ASC
 LIMIT $3
