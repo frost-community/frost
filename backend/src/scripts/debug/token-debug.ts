455a56852e92318dfcda180fa27aea15304747b4
marihachi <marihachi0620@gmail.com>
2024-09-08 21:48:50 +0900

    Backend: fix token repository (#126)

---
+++ backend/src/scripts/debug/token-debug.ts
@@ -0,0 +1,63 @@
+import { Container } from 'inversify';
+import { BACKEND_URSR_ID } from '../../constants/specialUserId';
+import { setupContainer } from '../../container/inversify.config';
+import { TYPES } from '../../container/types';
+import { AccessContext } from '../../modules/AccessContext';
+import { PrismaClient } from '@prisma/client';
+import { inspect } from 'util';
+import * as TokenRepository from '../../repositories/TokenRepository';
+import * as UserRepository from '../../repositories/UserRepository';
+import * as TokenService from '../../services/TokenService';
+
+async function run() {
+  const container = new Container();
+  setupContainer(container);
+
+  const ctx: AccessContext = { userId: BACKEND_URSR_ID };
+
+  // debugユーザーを取得。無ければ作る。
+  console.log('get debug user');
+  let user = await UserRepository.get({ name: 'debug' }, ctx, container);
+  if (user == null) {
+    console.log('create debug user');
+    user = await UserRepository.create({
+      name: 'debug',
+      displayName: 'Debug',
+      passwordAuthEnabled: false,
+    }, ctx, container);
+  }
+  ctx.userId = user.userId;
+
+  console.log('create');
+  const createResult = await TokenRepository.create({
+    tokenKind: 'access_token',
+    scopes: ['user.read', 'user.write'],
+    token: await TokenService.generateTokenValue(32),
+    userId: user.userId,
+  }, ctx, container);
+
+  console.log(inspect(createResult, { depth: 10 }));
+
+  console.log('get');
+  const getResult = await TokenRepository.get({
+    token: createResult.token,
+  }, ctx, container);
+
+  console.log(inspect(getResult, { depth: 10 }));
+
+  console.log('remove');
+  const removeResult = await TokenRepository.remove({
+    token: createResult.token,
+  }, ctx, container);
+
+  console.log(inspect(removeResult, { depth: 10 }));
+
+  console.log('finish');
+
+  const db = container.get<PrismaClient>(TYPES.db);
+  await db.$disconnect();
+}
+run()
+  .catch(err => {
+    console.error(err);
+  });
